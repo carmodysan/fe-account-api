84b0c1e6d907388cf367f9d59ea5e004d346fcf7
carmodysan <carmodysan@outlook.com>
2021-11-14 10:15:58 +0100

    Ajout du composant API Ajout du composant Apache Ajout du composant Uid Ajout de deux entités MonthlyAccount et Operation

---
+++ Procfile
@@ -0,0 +1,2 @@
+# Fichier de configuration pour indiquer à Heroku où se trouve le point d'entrée
+web: heroku-php-apache2 public/
---
+++ src/Entity/MonthlyAccount.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\MonthlyAccountRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=MonthlyAccountRepository::class)
+ */
+class MonthlyAccount
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="uuid", unique=true)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $month;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Operation::class, mappedBy="monthlyAccount", orphanRemoval=true)
+     */
+    private $operations;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+        $this->operations = new ArrayCollection();
+    }
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getMonth(): ?int
+    {
+        return $this->month;
+    }
+
+    public function setMonth(int $month): self
+    {
+        $this->month = $month;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Operation[]
+     */
+    public function getOperations(): Collection
+    {
+        return $this->operations;
+    }
+
+    public function addOperation(Operation $operation): self
+    {
+        if (!$this->operations->contains($operation)) {
+            $this->operations[] = $operation;
+            $operation->setMonthlyAccount($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOperation(Operation $operation): self
+    {
+        if ($this->operations->removeElement($operation)) {
+            // set the owning side to null (unless already changed)
+            if ($operation->getMonthlyAccount() === $this) {
+                $operation->setMonthlyAccount(null);
+            }
+        }
+
+        return $this;
+    }
+}
