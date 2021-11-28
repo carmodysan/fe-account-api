b553d3163e2fa37d516ef6479c0a0068fc8e8056
carmodysan <carmodysan@outlook.com>
2021-11-28 20:31:07 +0100

    Test

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\PeriodicOperationRepository;
 use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -16,14 +19,18 @@ use Symfony\Component\Serializer\SerializerInterface;
 class AuthController extends AbstractController
 {
     private $userRepository;
+    private $periodicOperationRepository;
     private $serializer;
     private $security;
+    private $logger;
 
-    public function __construct(UserRepository $userRepository, SerializerInterface $serializer, Security $security)
+    public function __construct(UserRepository $userRepository, PeriodicOperationRepository $periodicOperationRepository, SerializerInterface $serializer, Security $security, LoggerInterface $logger)
     {
         $this->userRepository = $userRepository;
+        $this->periodicOperationRepository = $periodicOperationRepository;
         $this->serializer = $serializer;
         $this->security = $security;
+        $this->logger = $logger;
     }
     
     /**
@@ -49,6 +56,26 @@ class AuthController extends AbstractController
         
         return new JsonResponse([
             $user
+        ], 200); 
+    }
+
+    /**
+     * @Route("/periodic_operations", name="user.preiod")
+     *
+     * @return JsonResponse
+     */
+    public function test(): JsonResponse
+    {
+        $currentUser = $this->security->getUser();
+        $user = $this->serializer->serialize($currentUser, 'json');
+        $obj = json_decode($user);
+        $this->logger->debug('====================>>>>>>>> '.$obj->{'id'});
+        $authorId = $obj->{'id'};
+        $periodicOperations = $this->periodicOperationRepository->findBy(['authorId' => $authorId]);
+        $po = $this->serializer->serialize($periodicOperations, 'json');
+
+        return new JsonResponse([
+            $po
         ], 200); 
     }
 }
