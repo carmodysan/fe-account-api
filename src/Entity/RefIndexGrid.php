be27b4cebe13baa9a32c3e756efe945d1fec10eb
carmodysan <carmodysan@outlook.com>
2022-01-09 16:22:00 +0100

    Modification Modèle de donnée grille indiciaire

--- src/Entity/RefIndexGrid.php
+++ src/Entity/RefIndexGrid.php
@@ -43,16 +43,6 @@ class RefIndexGrid
     private $grossIndex;
 
     /**
-     * @ORM\Column(type="date")
-     */
-    private $startDate;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $endDate;
-
-    /**
      * @ORM\ManyToOne(targetEntity=RefRank::class, inversedBy="refIndexGrids")
      * @ORM\JoinColumn(nullable=false)
      */
@@ -110,31 +100,7 @@ class RefIndexGrid
 
         return $this;
     }
-
-    public function getStartDate(): ?\DateTimeInterface
-    {
-        return $this->startDate;
-    }
-
-    public function setStartDate(\DateTimeInterface $startDate): self
-    {
-        $this->startDate = $startDate;
-
-        return $this;
-    }
-
-    public function getEndDate(): ?\DateTimeInterface
-    {
-        return $this->endDate;
-    }
-
-    public function setEndDate(\DateTimeInterface $endDate): self
-    {
-        $this->endDate = $endDate;
-
-        return $this;
-    }
-
+    
     public function getRank(): ?RefRank
     {
         return $this->rank;
